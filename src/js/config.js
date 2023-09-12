4beefc630e36659621e445e990675ee9779d84e9
jenner-df <jenner_defiesta@yahoo.com>
2023-09-12 20:02:42 +0800

    changed key

--- src/js/config.js
+++ src/js/config.js
@@ -4,5 +4,5 @@ export const API_URL_PLANT_DETAILS = 'https://perenual.com/api/species/details';
 export const API_URL_PLANT_CARE_GUIDE =
   'https://perenual.com/api/species-care-guide-list';
 export const TIMEOUT_SEC = 5;
-export const API_KEY = 'sk-Cium64ffc86b567452125';
+export const API_KEY = 'sk-FBF565005342d20ec2030';
 export const RESULTS_PER_PAGE = 10;
