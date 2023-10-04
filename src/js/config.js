5d1d0efafbda57a4e3adcc5ea9b65a068ebd6dca
Jenner <82255686+Jenner-DF@users.noreply.github.com>
2023-10-04 14:47:09 +0800

    Update key config.js

--- src/js/config.js
+++ src/js/config.js
@@ -3,5 +3,5 @@ export const API_URL_PLANT_DETAILS = 'https://perenual.com/api/species/details';
 export const API_URL_PLANT_CARE_GUIDE =
   'https://perenual.com/api/species-care-guide-list';
 export const TIMEOUT_SEC = 5;
-export const API_KEY = 'sk-FBF565005342d20ec2030';
+export const API_KEY = 'sk-JdEv651d09510aa042030';
 export const RESULTS_PER_PAGE = 10;
